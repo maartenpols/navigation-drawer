52bb4796b9fa4174066144dcde2b6bef3c31c320
Maarten Pols <pols.maarten@gmail.com>
2023-02-13 19:38:11 +0100

    First one

---
+++ app.js
@@ -0,0 +1,36 @@
+window.addEventListener("load", function (event) {
+  const accordion = document.getElementsByClassName("has-sub");
+  let lastClicked = null;
+
+  for (i = 0; i < accordion.length; i++) {
+    accordion[i].addEventListener("click", function () {
+      // Remove "active" class from all elements
+      for (j = 0; j < accordion.length; j++) {
+        accordion[j].classList.remove("active");
+      }
+      // Add or remove "active" class from the current element
+      if (this === lastClicked) {
+        lastClicked = null;
+      } else {
+        this.classList.add("active");
+        lastClicked = this;
+        if (
+          document
+            .getElementById("navigation-drawer")
+            .classList.contains("collapse")
+        ) {
+          document
+            .getElementById("navigation-drawer")
+            .classList.toggle("collapse");
+        }
+      }
+    });
+  }
+
+  let button = document.getElementById("button-collapse");
+  let sideBar = document.getElementById("navigation-drawer");
+
+  button.onclick = () => {
+    sideBar.classList.toggle("collapse");
+  };
+});
